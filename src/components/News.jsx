69d0d33c46ec1693b810ffae0bca1973a37f0326
henriquet kapema <henriquetkapema@gmail.com>
2024-11-08 02:42:23 +0100

    removing the apostrophy into the news component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -16,7 +16,7 @@ export default function News() {
     },[])
   return (
     <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2'>
-    <h4 className='font-bold text-xl px-4'>What's happening</h4>
+    <h4 className='font-bold text-xl px-4'>Whats happening</h4>
     {news.slice(0, articleNum).map((article) => (
       <div key={article.url}>
         <a href={article.url} target='_blank'>
